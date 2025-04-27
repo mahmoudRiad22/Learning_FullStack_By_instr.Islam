5fc1e9974bb7469686b021bbbe3c4c01b9d2212d
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-04-27 21:39:51 +0300

    recape try1

---
+++ BackEnd/Lec1/app.js
@@ -0,0 +1,6 @@
+// backend tack with nodejs
+// what do i need for nodejs:
+    // 1- basics for js the better you are at js the better you are in nodejs.
+            // variables, events, methods, jason, from json to object and vice versa.
+
+
