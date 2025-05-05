657f2c60f40a219e5bffd765f8406b582f6a53cf
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-05-05 08:22:09 +0300

    Finished the todo list in lec 27, great stuff ngl

--- JavaScript/Lec27-js-2/js/script.js
+++ JavaScript/Lec27-js-2/js/script.js
@@ -79,13 +79,13 @@ console.log(var1 === null);
 // &&   ||  !
 ///////////////////////////////////
 
-const result = document.querySelector(".resultsection .result");
-const addition = document.getElementById("addition");
+// const result = document.querySelector(".resultsection .result");
 
-// const div1btn = document.getElementById("div1btn");
-// const div2btn = document.getElementById("div2btn");
+///////////////////////////////////////////////////////////
+///////////////////// Functions //////////////////////////
 
 function SubmitData() {
+  console.log("inside the submitdata function");
   const input1 = document.getElementById("div1input");
   const input2 = document.getElementById("div2input");
   const input3 = document.getElementById("div3input");
@@ -98,21 +98,264 @@ function SubmitData() {
   const value4 = input4.value;
   const value5 = input5.value;
 
-  console.log("Div1 Number is:", value1);
-  console.log("Div2 Number is:", value2);
-  console.log("Div3 Number is:", value3);
-  console.log("Div4 Number is:", value4);
-  console.log("Div5 Number is:", value5);
+  // console.log(value1,value2, value3,value4,value5);
+
+  document.querySelector(".resultsection").insertAdjacentHTML(
+    "beforeend",
+    `<span>You Entered these numbers:<br>
+  ${value1},
+  ${value2},
+  ${value3},
+  ${value4},
+  ${value5},
+  <span/> <br>`
+  );
 }
+////////////////////////////////////
+function addition() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
 
-document.getElementById("div1btn").onclick = SubmitData;
-document.getElementById("div2btn").onclick = SubmitData;
-document.getElementById("div3btn").onclick = SubmitData;
-document.getElementById("div4btn").onclick = SubmitData;
-document.getElementById("div5btn").onclick = SubmitData;
-// console.log(`Number Entered in Div1: ${value}`);
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
 
+  const total =
+    Number(value1) +
+    Number(value2) +
+    Number(value3) +
+    Number(value4) +
+    Number(value5);
 
+  // console.log(`Total: ${total}`);
+
+  document
+    .querySelector(".resultsection")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<span>Addition Total = ${total}<span/> <br>`
+    );
+}
+////////////////////////////////////
+
+function subtraction() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+
+  const total =
+    Number(value1) -
+    Number(value2) -
+    Number(value3) -
+    Number(value4) -
+    Number(value5);
+
+  document
+    .querySelector(".resultsection")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<span>Subtraction Total: ${total}<span/> <br>`
+    );
+}
+////////////////////////////////////
+
+function division() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+
+  const total =
+    Number(value1) /
+    Number(value2) /
+    Number(value3) /
+    Number(value4) /
+    Number(value5);
+
+  document
+    .querySelector(".resultsection")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<span>Division Total: ${total}<span/> <br>`
+    );
+}
+////////////////////////////////////
+
+function multiplication() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+
+  const total =
+    Number(value1) *
+    Number(value2) *
+    Number(value3) *
+    Number(value4) *
+    Number(value5);
+
+  document
+    .querySelector(".resultsection")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<span>Multiplication Total: ${total}<span/> <br>`
+    );
+}
+////////////////////////////////////
+
+function modulus() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+  const modulus = document.getElementById("modulusinput");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+  const modulusvalue = modulus.value;
+
+  document.querySelector(".resultsection").insertAdjacentHTML(
+    "beforeend",
+    `<span>Modulus All By ${modulusvalue}:<br>
+      ${Number(value1) % Number(modulusvalue)},
+      ${Number(value2) % Number(modulusvalue)},
+      ${Number(value3) % Number(modulusvalue)},
+      ${Number(value4) % Number(modulusvalue)},
+      ${Number(value5) % Number(modulusvalue)}
+    <span/> <br>`
+  );
+}
+////////////////////////////////////
+
+function increment() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+
+  const total =
+    Number(value1) *
+    Number(value2) *
+    Number(value3) *
+    Number(value4) *
+    Number(value5);
+
+  document.querySelector(".resultsection").insertAdjacentHTML(
+    "beforeend",
+    `<span>Increment All: <br>
+    ${Number(value1) + 1}, ${Number(value2) + 1}, ${Number(value3) + 1}, 
+    ${Number(value4) + 1}, ${Number(value5) + 1}
+    <span/> <br>`
+  );
+}
+////////////////////////////////////
+
+function decrement() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+
+  const total =
+    Number(value1) *
+    Number(value2) *
+    Number(value3) *
+    Number(value4) *
+    Number(value5);
+
+  document.querySelector(".resultsection").insertAdjacentHTML(
+    "beforeend",
+    `<span>Decrement All: <br>
+    ${Number(value1) - 1}, ${Number(value2) - 1}, ${Number(value3) - 1}, 
+    ${Number(value4) - 1}, ${Number(value5) - 1}
+    <span/> <br>`
+  );
+}
+////////////////////////////////////
+
+function exponentioal() {
+  const input1 = document.getElementById("div1input");
+  const input2 = document.getElementById("div2input");
+  const input3 = document.getElementById("div3input");
+  const input4 = document.getElementById("div4input");
+  const input5 = document.getElementById("div5input");
+  const expo = document.getElementById("expoinput");
+
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const value4 = input4.value;
+  const value5 = input5.value;
+  const expoval = expo.value;
+
+  document.querySelector(".resultsection").insertAdjacentHTML(
+    "beforeend",
+    `<span>exponentioal All: <br>
+    ${Number(value1) ** Number(expoval)}, ${
+      Number(value2) ** Number(expoval)
+    }, ${Number(value3) ** Number(expoval)}, 
+    ${Number(value4) ** Number(expoval)}, ${Number(value5) ** Number(expoval)}
+    <span/> <br>`
+  );
+}
+////////////////////////////////////
+
+///////////////////////////////////////////////////////
+///////////////////// EVENTS //////////////////////////
+document.getElementById("submitbtn").onclick = SubmitData;
+document.getElementById("btn1").onclick = addition;
+document.getElementById("btn2").onclick = subtraction;
+document.getElementById("btn3").onclick = division;
+document.getElementById("btn4").onclick = multiplication;
+document.getElementById("btn5").onclick = modulus;
+document.getElementById("btn6").onclick = increment;
+document.getElementById("btn7").onclick = decrement;
+document.getElementById("btn8").onclick = exponentioal;
 
 /////////////////// TODO //////////////////////////
 // excuate addtion operation on all input values
@@ -138,5 +381,3 @@ document.getElementById("div5btn").onclick = SubmitData;
 
 // excuate eponential operation on all input values
 // output the result in the result div as a span
-
-
