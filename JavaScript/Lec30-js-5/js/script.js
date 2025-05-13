0912a67e69fa3ff4653860a0c1c8cbbc7892e976
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-05-13 12:44:19 +0300

    Pause on Lec30 [No Internet], move to Lec31

---
+++ JavaScript/Lec30-js-5/js/script.js
@@ -0,0 +1,24 @@
+// Ajax: Asynchronous js and xml
+
+//  waht exactly happens when i search for a website in google?
+
+(function httprequest() {
+  const new_httpReq = new XMLHttpRequest();
+  console.log(new_httpReq);
+})();
+
+/* 
+readystate:
+0:  request not initialized;
+1:  request is sent out successfully;
+2:  request is recieved successfully;
+3:  request is processed;
+4:  request is done and response is ready;
+*/
+
+/* 
+status:
+200: ok;
+403: Forbidden;
+404: not found; // timeout;
+*/
